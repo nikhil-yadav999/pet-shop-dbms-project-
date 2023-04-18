39e7deea9d4b9913b55663aa620af7728b76a157
brockhere <131190086+brockhere@users.noreply.github.com>
2023-04-18 23:46:47 +0530

    Update pet.php

--- source code/pet.php
+++ source code/pet.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "Petshop_management";
+$password = "1234";
+$dbname = "petshop_management";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
